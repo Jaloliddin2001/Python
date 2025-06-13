2f83f67e7fa49de8e26e86bc3bb42c435c36786c
Jaloliddin2001 <tulamurodovjaloliddin973@gmail.com>
2025-06-13 16:35:05 +0500

    Add files via upload

---
+++ lesson_11/homework/main.py
@@ -0,0 +1,12 @@
+import math_operations
+import string_utils
+
+# Math operations
+print(math_operations.add(10, 5))        # 15
+print(math_operations.subtract(10, 5))   # 5
+print(math_operations.multiply(10, 5))   # 50
+print(math_operations.divide(10, 2))     # 5.0
+
+# String utilities
+print(string_utils.reverse_string("hello"))  # "olleh"
+print(string_utils.count_vowels("hello"))    # 2
